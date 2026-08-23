2e04f4433101bdb79cb1fe004f8d96434354f89b
Vitalii Radaiev <hf.chelenger@gmail.com>
2026-08-23 14:41:04 +0300

    fix

--- blocks/block_multicontent.php
+++ blocks/block_multicontent.php
@@ -12,7 +12,7 @@ if (!$data['section_utils']['is_hide']):
 
     $is_show_colour_bg = $data['is_show_colour_bg'] ?? false;
     $bg_color = $data['bg-color'];
-    $columns = $data['columns'] ?? [];
+    $columns = !empty($data['columns']) ? $data['columns'] : [];
 ?>
     <section <?= $is_show_colour_bg ? 'style="background-color:' . $bg_color . ';"' : '' ?> <?= get_section_id($data) ?> class="multicontent <?= $is_show_colour_bg ? 'multicontent--bg' : '' ?> <?= get_section_space_top($data) ?>">
         <div class="container multicontent__container">
