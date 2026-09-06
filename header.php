8d0ac080f00524144f642f0d3c8727bdf228d81d
Vitalii Radaiev <hf.chelenger@gmail.com>
2026-09-06 12:34:13 +0300

    fixes

--- header.php
+++ header.php
@@ -6,6 +6,9 @@ $whatsapp = get_field('whatsapp', 'option');
 $viber = get_field('viber', 'option');
 $header_logo = get_field('header_logo', 'option');
 $text_cabinet = get_field('text_cabinet', 'option');
+$email = get_field('email', 'option');
+$contact_button = get_field('sontact_button', 'option')['button'] ?? [];
+$text_contact_us = get_field('text_contact_us', 'option');
 ?>
 
 <!DOCTYPE html>
@@ -44,16 +47,6 @@ $text_cabinet = get_field('text_cabinet', 'option');
                         </a>
                     </div>
                 <?php endif; ?>
-
-                <?php
-                // wp_nav_menu(array(
-                //     'theme_location' => 'headerMenuLocation',
-                //     'container' => 'nav',
-                //     'container_class' => 'header__menu-wrap',
-                //     'menu_class' => 'header__menu',
-                //     'fallback_cb' => false,
-                // ));
-                ?>
 
                 <nav class="header__menu-wrap">
                     <ul id="menu-header-menu" class="header__menu">
@@ -125,6 +118,53 @@ $text_cabinet = get_field('text_cabinet', 'option');
                         <?php endif; ?>
                     </ul>
                 </nav>
+
+
+                <?php if (check($whatsapp) || check($viber) || check($email) || check($contact_button['button_text'] ?? null)): ?>
+                    <div class="header-contacts">
+                        <button type="button" class="header-contacts__toggle" aria-expanded="false" aria-haspopup="true">
+                            <?= $text_contact_us ?>
+                        </button>
+
+                        <div class="header-contacts__dropdown">
+                            <ul class="header-contacts__list">
+                                <?php if (check($whatsapp)): ?>
+                                    <li>
+                                        <a href="https://wa.me/<?= cleanPhoneNumber($whatsapp) ?>" target="_blank">
+                                            <img src="<?= get_theme_file_uri() . '/dist/images/whatsapp.svg' ?>" alt="WhatsApp">
+                                            <?= $whatsapp ?>
+                                        </a>
+                                    </li>
+                                <?php endif; ?>
+
+                                <?php if (check($viber)): ?>
+                                    <li>
+                                        <a href="viber://chat?number=%2B<?= cleanPhoneNumber($viber) ?>" target="_blank">
+                                            <img src="<?= get_theme_file_uri() . '/dist/images/viber.svg' ?>" alt="Viber">
+                                            <?= $viber ?>
+                                        </a>
+                                    </li>
+                                <?php endif; ?>
+
+                                <?php if (check($email)): ?>
+                                    <li class="header-contacts__email">
+                                        <a href="mailto:<?= antispambot($email) ?>">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="15" viewBox="0 0 18 15" fill="currentColor" aria-hidden="true">
+                                                <path d="M17.982 12.928c0 .434-.158.809-.474 1.125a1.537 1.537 0 0 1-1.125.475H1.6c-.434 0-.809-.158-1.125-.475A1.537 1.537 0 0 1 0 12.928V1.995C0 1.56.158 1.186.475.87A1.537 1.537 0 0 1 1.6.395h14.783c.433 0 .808.158 1.125.475.316.316.474.691.474 1.125v10.933Zm-1.6-11.25H1.6a.303.303 0 0 0-.22.097.303.303 0 0 0-.097.22c0 .574.132 1.104.396 1.59a3.95 3.95 0 0 0 1.08 1.257l2.013 1.591c.674.533 1.345 1.064 2.013 1.59.27.224.615.508 1.037.853.422.346.809.519 1.16.519H9c.352 0 .738-.173 1.16-.519.422-.345.768-.63 1.037-.852l2.013-1.59 2.013-1.592c.328-.246.656-.612.984-1.098.328-.487.492-.94.492-1.363 0-.129-.006-.278-.017-.448-.012-.17-.112-.255-.3-.255Zm.317 3.533a6.094 6.094 0 0 1-.703.668 264.93 264.93 0 0 0-2.144 1.67 97.422 97.422 0 0 0-2.127 1.723c-.375.316-.791.63-1.248.94-.457.31-.95.466-1.477.466h-.018a2.57 2.57 0 0 1-1.476-.466 15.03 15.03 0 0 1-1.248-.94A97.611 97.611 0 0 0 4.13 7.549a161.232 161.232 0 0 0-2.162-1.67 17.74 17.74 0 0 1-.352-.325 6.121 6.121 0 0 1-.334-.343v7.717c0 .082.032.155.097.22a.303.303 0 0 0 .22.097h14.783a.303.303 0 0 0 .22-.097.302.302 0 0 0 .096-.22V5.211Z" />
+                                            </svg>
+                                            <?= antispambot($email) ?>
+                                        </a>
+                                    </li>
+                                <?php endif; ?>
+                            </ul>
+
+                            <?php get_template_part(get_part_path('button'), null, [
+                                'button_data' => $contact_button,
+                                'classes'     => 'header-contacts__btn',
+                            ]); ?>
+                        </div>
+                    </div>
+                <?php endif; ?>
 
                 <div class="header__nav-mobile">
                     <button class="openMenu" aria-label="Open menu">
